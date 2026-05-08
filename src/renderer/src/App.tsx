fae44583ba75b0c386315a93a53345a6269dd0d6
Yoshinao Inoguchi <nao7sep@gmail.com>
2026-05-08 11:46:39 +0900

    feat: remove dynamic background hue effect and update background gradients in styles

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import { Layout } from './components/Layout'
 import { CliJobsPanel } from './components/CliJobsPanel'
 import { ConfirmProvider } from './context/ConfirmContext'
@@ -8,24 +7,7 @@ import { SelectionProvider } from './context/SelectionContext'
 import { SettingsProvider } from './context/SettingsContext'
 import './styles.css'
 
-const BG_HUE_CYCLE_MS = 12000
-
 export function App(): React.JSX.Element {
-  useEffect(() => {
-    let frameId = 0
-    const root = document.documentElement
-    const start = performance.now()
-
-    const updateHue = (now: number): void => {
-      const hue = (((now - start) % BG_HUE_CYCLE_MS) / BG_HUE_CYCLE_MS) * 360
-      root.style.setProperty('--bg-hue', hue.toFixed(2))
-      frameId = requestAnimationFrame(updateHue)
-    }
-
-    frameId = requestAnimationFrame(updateHue)
-    return () => cancelAnimationFrame(frameId)
-  }, [])
-
   return (
     <CliJobsProvider>
       <ConfirmProvider>
